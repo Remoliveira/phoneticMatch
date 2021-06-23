95c954a24237da8935670b22c8dbce5284be4427
Remo de Oliveira Gresta <remoogg@gmail.com>
2021-06-23 01:21:13 -0300

    changes

--- metaphone.py
+++ metaphone.py
@@ -1,7 +1,25 @@
 import phonetics as ph
 import fuzzy
-import requests
+from bs4 import BeautifulSoup
 import jellyfish
+import requests
+
+url = "https://www.thoughtco.com/hard-to-pronounce-words-4156950"
+
+page = requests.get(url)
+
+pageSoup = BeautifulSoup(page.content, 'html.parser')
+
+hardWords =  pageSoup.find_all('span',class_="mntl-sc-block-heading__text")
+
+unWords = []
+for words in hardWords:
+    # print(words.text)
+    unWords.append(words.text)
+
+unWords.pop()
+
+print(unWords)
 
 
 # print(ph.dmetaphone("door"))
@@ -14,7 +32,8 @@ soundex = fuzzy.Soundex(4)
 # dmeta = fuzzy.DMetaphone()
 # print(dmeta('xsaiwwwdmo'))
 
-print(jellyfish.metaphone('Antidisestablishmentarianism'))
-print(jellyfish.soundex('Anathema'))
+# print(jellyfish.metaphone('Antidisestablishmentarianism'))
+# print(jellyfish.soundex('Anathema'))
 
-print(jellyfish.levenshtein_distance('response','answer'))
+# print(jellyfish.levenshtein_distance('response','answer'))
+
